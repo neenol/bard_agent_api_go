46c9d3735c4e3724d52f89b67cfc0bd60dd9f378
Nino Lambert <anthony.lamb001@gmail.com>
2022-11-15 10:38:40 -0500

    parse events from request body

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//TODO: better typing for events
 //odd, looks like properties need to have the first letter capitalized
 //in order for gin's binding functions to work...
+//[]map[string]interface{} means that its an array of map values. the
+//maps have strings as keys. interface{} means the value can be anything
 type RecordBody struct {
 	SessionId string `binding:"required"`;
-	Events []string ` binding:"required"`;
+	Events []map[string]interface{} `binding:"required"`;
 	MAX_IDLE_TIME uint32 `binding:"required"`;
 }
 
@@ -45,10 +46,11 @@ func main() {
 		//tried to use bindHeader to do this and couldn't get it to work
 		appName := c.GetHeader("appname")
 		if appName == "" {
-			send404Res(c, "bad request: no appname header")
+			send404Res(c, "Bad request: no appname header")
 			return
 		}
-
+		fmt.Printf("c: %v\n", c)
+		
 		//get the body
 		var body RecordBody
 		if err := c.BindJSON(&body); err != nil {
@@ -56,14 +58,45 @@ func main() {
 			send404Res(c, msg)
 			return
 		}
-		sessionId := body.SessionId
-		events := body.Events
-		maxIdleTime := body.MAX_IDLE_TIME
-		fmt.Println("the gangs all here!", sessionId, events, maxIdleTime)
+
+		//handle our events
+		if err := handleEvents(c, body); err != nil {
+			msg := fmt.Sprintf("Event handling error. %s", err)
+			send500Res(c, msg)
+			return
+		} else {
+			send200Res(c, "thanks")
+			return
+		}
 	})
 	r.Run(":3001")
 }
 
 func send404Res(c *gin.Context, msg string) {
 	c.JSON(404, msg)
+}
+
+func send500Res(c *gin.Context, msg string) {
+	c.JSON(404, msg)
+}
+
+func send200Res(c *gin.Context, msg string) {
+	c.JSON(200, msg)
+}
+
+func handleEvents(c *gin.Context, body RecordBody) error {
+	sessionId := body.SessionId
+	events := body.Events
+	maxIdleTime := body.MAX_IDLE_TIME
+	fmt.Println("the gangs all here!", sessionId, events, maxIdleTime)
+	for i := 0; i < len(events); i++ {
+		//we can access all fields of the event using this event object.
+		//But, go doesn't know that event["data"] is another map, so we 
+		//need to tell it that. If we try to access non-existent values, 
+		//they come up as nil
+		var event = events[i]
+		var data = event["data"].(map[string]interface{})
+		fmt.Println("parsed data", data)
+	}
+	return nil
 }
