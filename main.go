93d447cdad4f0a73f0efd0015afb4e2bcadfece9
Nino Lambert <anthony.lamb001@gmail.com>
2022-11-14 19:31:54 -0500

    adding basic http functionality to main.go

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 //getting an error here, but it must be something with the extension
 //because the code runs just fine
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	//basic path
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	//getting a url path param
+	r.GET("/ping/:payload", func(c *gin.Context) {
+		payload:= c.Param("payload")
+		c.JSON(200, gin.H{
+			"payload": payload,
+		})
+		fmt.Println("payload is ", payload)
+	})
+	//getting string query params: expecting /query?database=postgres&queue=rabbit
+	r.GET("/query", func(c *gin.Context) {
+		//gets the query value for 'database' and returns the 2nd arg if its not there
+		db := c.DefaultQuery("database", "N/A")
+		queue := c.Query("queue")
+		fmt.Println("db is", db, "and queue is", queue)
+	})
+	r.Run(":3000")
 }
